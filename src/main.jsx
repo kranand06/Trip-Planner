5c08ee83aea8c3a83e947ee535dbca1064505763
Kumar Anand <kranand9430@gmail.com>
2025-01-08 18:04:11 +0530

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <App />,
+      },
+    ]
+  }
+    ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
